b4f747d2e7a332e43a95e12b9f44977e485aeb33
RushikeshNaidu <rushi.naidu1@gmail.com>
2018-03-20 12:16:15 -0400

    Update Customer.R

--- Customer.R
+++ Customer.R
@@ -11,18 +11,18 @@ for (i in 2:15) within_sum_of_squares[i] <- sum(kmeans(cleandata,
                                      centers=i)$withinss)
 plot(1:15,within_sum_of_squares, type="b", xlab="Number of Clusters",
      ylab="Within groups sum of squares")
-#This does not give a strong elbow, but intuition shows that there is no significant drop after 6 clusters. 
+#This does not give a strong elbow, but the graph shows that there is no significant drop after 6 clusters.. 
 fit <- kmeans(cleandata, 6) # fit the model
-aggregate(cleandata,by=list(fit$cluster),FUN=mean) # get cluster means
-cleandata <- data.frame(cleandata, fit$cluster) #append cluster assignment
+aggregate(cleandata,by=list(fit$cluster),FUN=mean) # getting cluster means
+cleandata <- data.frame(cleandata, fit$cluster) #appending cluster 
 library(cluster)
 clusplot(cleandata, fit$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
 sampled <- sample(1:dim(cleandata)[1], 50)
 Sample_final <- cleandata[sampled,]
 Sample_final$Region <- NULL
 Sample_final$Channel <- NULL
-d <- dist(Sample_final, method = "euclidean") # distance matrix
+d <- dist(Sample_final, method = "euclidean") 
 fit <- hclust(d, method="ward") 
-plot(fit) # display dendogram
+plot(fit) 
 groups <- cutree(fit, k=6) # cut tree into 6 clusters
 rect.hclust(fit, k=6, border="red") # draw dendogram with red borders around the 6 clusters
